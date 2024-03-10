3b48d72e7936efdbd3c6476bb7d63ca3e51f2dbc
Leopold <leopolddag@gmail.com>
2024-03-10 10:45:55 +0100

    correction

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -6,7 +6,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i >= 0; i++) {
-			System.out.println("Hello bugs !");
+			System.out.println("Hello bugs !!!");
 		}
 	}
 }
