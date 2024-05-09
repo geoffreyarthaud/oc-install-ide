7bf1933469c8518d4ec81246263ecb4f954cb7df
JOSUE N'GORAN <JOSUE N'GORAN@192.168.117.1>
2024-05-09 00:09:20 +0000

    Correction des erreurs de syntaxe et des erreurs d'exécution

---
+++ src/main/java/com/openclassrooms/ideinstall1/BasicExample.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.ideinstall1;
+
+public class BasicExample {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 0; i < 5; i++) {
+			System.out.println("Hello bugs !");
+		}
+	}
+}
