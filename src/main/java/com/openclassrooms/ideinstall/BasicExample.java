c22f395c2bf08e856805503471780bb100445cce
Leopold <leopolddag@gmail.com>
2024-06-02 11:11:59 +0200

    correction de la syntaxe et des erreurs d'execution

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,9 +5,9 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
-		for (i = 0; i >= 0; i++) {
+		for (i = 0; i < 3; i++) {
 			System.out.println("Hello bugs !!!");
-			// ok encore
 		}
 	}
 }
+ 
